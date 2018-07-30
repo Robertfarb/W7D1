5cb18debab51a892ebe35a0c54136667ad2c7e94
roberfarb <farb.robert@gmail.com>
2018-07-30 12:10:10 -0700

    Setup file tree

---
+++ todos/frontend/todos.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import configureStore from ''
+
+document.addEventListener('DOMContentLoaded', () => {
+  debugger
+  const root = document.findElementById('root');
+  ReactDOM.render(<h1>react workss</h1>, root);
+  }
+);
